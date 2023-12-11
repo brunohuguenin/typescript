ca6cc4c750979f4d1b0c1281a940f932cd5be3a3
Brunohugueninn <brunohugueninn@gmail.com>
2023-12-11 17:24:24 -0300

    aula 3

---
+++ string-number-boolean/script.js
@@ -0,0 +1,17 @@
+"use strict";
+// const frase = 'Front End'
+// const preco = 500
+// const condi = preco > 100 
+// console.log(typeof frase)
+// console.log(typeof preco)
+// if (typeof frase === "string") {
+//   console.log("Frase é uma String")
+// } else {
+//   console.log("Frase não é uma String")
+// }
+const frase1 = new String('Front End');
+const frase2 = String('Front End');
+const frase3 = 'Front End';
+console.log(typeof frase1);
+console.log(typeof frase2);
+console.log(typeof frase3);
