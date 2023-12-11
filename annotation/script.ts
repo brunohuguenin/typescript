3fd7386369d994a815c633a6a5127ca5a2f88260
Brunohugueninn <brunohugueninn@gmail.com>
2023-12-11 16:51:09 -0300

    aula 2

---
+++ annotation/script.ts
@@ -0,0 +1,32 @@
+// let produto = 'livro'
+// let preco = 200
+
+// const carro:  {
+//   marca: string;
+//   portas: number;
+// } = {
+//   marca: "Audi",
+//   portas: 5,
+// }
+
+// const barato: boolean | string = preco < 400 ? true : 'Produto caro'
+// console.log(barato)
+
+// function somar(a: number, b: number) {
+//   return a + b
+// }
+// console.log(somar(17, 29))
+
+// const nintendo = {
+//   nome: "Nintendo",
+//   preco: "2000"
+// }
+
+// function transformaPreco(produto: {nome: string; preco: string}) {
+//   produto.preco = "R$ " + produto.preco;
+//   return produto
+// }
+
+// const produtoNovo = transformaPreco(nintendo)
+// console.log(produtoNovo)
+
