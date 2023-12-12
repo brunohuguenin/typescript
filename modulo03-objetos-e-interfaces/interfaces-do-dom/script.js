4d5acc7d535aeaa86d17505da5375d5bd2af5c75
Brunohugueninn <brunohugueninn@gmail.com>
2023-12-12 17:10:21 -0300

    aula 12

--- modulo03-objetos-e-interfaces/interfaces-do-dom/script.js
+++ modulo03-objetos-e-interfaces/interfaces-do-dom/script.js
@@ -1,5 +1,11 @@
 "use strict";
-const video = document.querySelector('#videoprincipal');
-if (video instanceof HTMLVideoElement) {
-    console.log(video.volume);
-}
+// const video = document.querySelector('#videoprincipal')
+// if (video instanceof HTMLVideoElement) {
+//   console.log(video.volume)
+// }
+const links = document.querySelectorAll('.link');
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        console.log(link.href);
+    }
+});
--- modulo03-objetos-e-interfaces/interfaces-do-dom/script.ts
+++ modulo03-objetos-e-interfaces/interfaces-do-dom/script.ts
@@ -1,5 +1,14 @@
-const video = document.querySelector('#videoprincipal')
+// const video = document.querySelector('#videoprincipal')
 
-if (video instanceof HTMLVideoElement) {
-  console.log(video.volume)
-}
+// if (video instanceof HTMLVideoElement) {
+//   console.log(video.volume)
+// }
+
+
+const links = document.querySelectorAll('.link')
+
+links.forEach((link) => {
+  if (link instanceof HTMLAnchorElement) {
+    console.log(link.href)
+  }
+})
