3d2ba089a355d341d02db49d0d60abbf88f21343
Brunohugueninn <brunohugueninn@gmail.com>
2023-12-18 16:05:38 -0300

    aula 19

--- modulo03-objetos-e-interfaces/user-typeGuard/script.js
+++ modulo03-objetos-e-interfaces/user-typeGuard/script.js
@@ -1,4 +1,9 @@
 "use strict";
+// async function fetchProduto() {
+//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
+//   const json = await response.json();
+//   handleCursos(json);
+// };
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,29 +13,43 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// fetchProduto();
+// function handleCursos(data: unknown) {
+//   if (data instanceof Array) {
+//     console.log('É uma instancia de array')
+//   }
+//   if (Array.isArray(data)) {
+//     console.log('É array')
+//   }
+// }
+// function isString(value: unknown): value is string {
+//   return typeof value === 'string';
+// }
+// function handleData(data: unknown) {
+//   if (isString(data)) {
+//     console.log(data.toLocaleLowerCase());
+//   }
+// }
+// handleData('Valeu, Natalina')
 function fetchProduto() {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield fetch('https://api.origamid.dev/json/cursos.json');
         const json = yield response.json();
-        handleCursos(json);
+        handleProduto(json);
     });
 }
 ;
 fetchProduto();
-function handleCursos(data) {
-    if (data instanceof Array) {
-        console.log('É uma instancia de array');
+function isProduto(value) {
+    if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
+        return true;
     }
-    if (Array.isArray(data)) {
-        console.log('É array');
-    }
-}
-function isString(value) {
-    return typeof value === 'string';
-}
-function handleData(data) {
-    if (isString(data)) {
-        console.log(data.toLocaleLowerCase());
+    else {
+        return false;
     }
 }
-handleData('Valeu, Natalina');
+function handleProduto(data) {
+    if (isProduto(data)) {
+        console.log(data.nome);
+    }
+}
--- modulo03-objetos-e-interfaces/user-typeGuard/script.ts
+++ modulo03-objetos-e-interfaces/user-typeGuard/script.ts
@@ -1,28 +1,55 @@
+// async function fetchProduto() {
+//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
+//   const json = await response.json();
+//   handleCursos(json);
+// };
+
+// fetchProduto();
+
+// function handleCursos(data: unknown) {
+//   if (data instanceof Array) {
+//     console.log('É uma instancia de array')
+//   }
+//   if (Array.isArray(data)) {
+//     console.log('É array')
+//   }
+// }
+
+// function isString(value: unknown): value is string {
+//   return typeof value === 'string';
+// }
+
+// function handleData(data: unknown) {
+//   if (isString(data)) {
+//     console.log(data.toLocaleLowerCase());
+//   }
+// }
+
+// handleData('Valeu, Natalina')
+
 async function fetchProduto() {
   const response = await fetch('https://api.origamid.dev/json/cursos.json');
   const json = await response.json();
-  handleCursos(json);
+  handleProduto(json);
 };
 
 fetchProduto();
 
-function handleCursos(data: unknown) {
-  if (data instanceof Array) {
-    console.log('É uma instancia de array')
-  }
-  if (Array.isArray(data)) {
-    console.log('É array')
-  }
+interface Produto {
+  nome: string;
+  preco: number;
 }
 
-function isString(value: unknown): value is string {
-  return typeof value === 'string';
-}
-
-function handleData(data: unknown) {
-  if (isString(data)) {
-    console.log(data.toLocaleLowerCase());
+function isProduto(value: unknown): value is Produto {
+  if(value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
+    return true;
+  } else {
+    return false
   }
 }
 
-handleData('Valeu, Natalina')
+function handleProduto(data: unknown) {
+  if (isProduto(data)) {
+    console.log(data.nome)
+  }
+}
