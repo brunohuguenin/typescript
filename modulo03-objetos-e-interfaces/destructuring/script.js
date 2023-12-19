3a17908ebcef94e174b6c57ddbd92d2f02072d47
Brunohugueninn <brunohugueninn@gmail.com>
2023-12-19 16:09:19 -0300

    aula 23

--- modulo03-objetos-e-interfaces/destructuring/script.js
+++ modulo03-objetos-e-interfaces/destructuring/script.js
@@ -1,13 +1,25 @@
 "use strict";
-const { body } = document;
-function handleData({ nome, preco }) {
-    nome.includes("book");
-    preco === null || preco === void 0 ? void 0 : preco.toFixed();
+// const {body} : {body: HTMLElement} = document;
+// function handleData({nome, preco}: {nome:string; preco?: number}){
+//   nome.includes("book");
+//   preco?.toFixed();
+// }
+// handleData({
+//   nome: "Notebook",
+// })
+// function handleClick({currentTarget, pageX}: {currentTarget: EventTarget | null; pageX: number}) {
+//   console.log(currentTarget, pageX)
+// }
+// document.documentElement.addEventListener("click", handleClick);
+function comparar(tipo, ...numeros) {
+    console.log(tipo);
+    console.log(numeros);
+    if (tipo === "menor") {
+        return Math.min(...numeros);
+    }
+    if (tipo === "maior") {
+        return Math.max(...numeros);
+    }
 }
-handleData({
-    nome: "Notebook",
-});
-function handleClick({ currentTarget, pageX }) {
-    console.log(currentTarget, pageX);
-}
-document.documentElement.addEventListener("click", handleClick);
+console.log(comparar("menor", 4, 5, 3, 4, 17));
+console.log(comparar("maior", 4, 5, 3, 4, 17));
--- modulo03-objetos-e-interfaces/destructuring/script.ts
+++ modulo03-objetos-e-interfaces/destructuring/script.ts
@@ -1,16 +1,32 @@
-const {body} : {body: HTMLElement} = document;
+// const {body} : {body: HTMLElement} = document;
 
-function handleData({nome, preco}: {nome:string; preco?: number}){
-  nome.includes("book");
-  preco?.toFixed();
+// function handleData({nome, preco}: {nome:string; preco?: number}){
+//   nome.includes("book");
+//   preco?.toFixed();
+// }
+
+// handleData({
+//   nome: "Notebook",
+// })
+
+// function handleClick({currentTarget, pageX}: {currentTarget: EventTarget | null; pageX: number}) {
+//   console.log(currentTarget, pageX)
+// }
+
+// document.documentElement.addEventListener("click", handleClick);
+
+function comparar(tipo: string, ...numeros: number[]) {
+  console.log(tipo);
+  console.log(numeros)
+  if (tipo === "menor") {
+    return Math.min(...numeros);
+  }
+  if (tipo === "maior") {
+    return Math.max(...numeros);
+  }
 }
 
-handleData({
-  nome: "Notebook",
-})
 
-function handleClick({currentTarget, pageX}: {currentTarget: EventTarget | null; pageX: number}) {
-  console.log(currentTarget, pageX)
-}
 
-document.documentElement.addEventListener("click", handleClick);
+console.log(comparar("menor", 4,5,3,4,17));
+console.log(comparar("maior", 4,5,3,4,17));
